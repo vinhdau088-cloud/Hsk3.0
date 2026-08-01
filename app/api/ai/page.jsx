8fd538b33efaa06f5f9183e1a6b285d4d106a236
vinhdau088-cloud <vinhdau088@gmail.com>
2026-08-01 19:18:55 +0800

    Create page.jsx

---
+++ app/api/ai/page.jsx
@@ -0,0 +1,20 @@
+import ChatBox from "@/components/ChatBox";
+
+
+export default function AI(){
+
+return (
+
+<main style={{padding:25}}>
+
+<h1>
+🤖 AI Giáo viên tiếng Trung
+</h1>
+
+<ChatBox/>
+
+</main>
+
+)
+
+}
