52eb2eb8f93c349313fd960afb4a4653efbb4d02
vinhdau088-cloud <vinhdau088@gmail.com>
2026-08-01 18:38:03 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,28 @@
+import "./globals.css";
+
+
+export const metadata = {
+
+title:"ChineseMaster AI",
+description:"Học tiếng Trung HSK TOCFL"
+
+};
+
+
+export default function RootLayout({children}){
+
+return (
+
+<html lang="vi">
+
+<body>
+
+{children}
+
+</body>
+
+</html>
+
+)
+
+}
