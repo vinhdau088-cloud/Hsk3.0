d54055ac2afa3397fce11a231e007bf1d0141338
vinhdau088-cloud <vinhdau088@gmail.com>
2026-08-01 21:24:26 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,27 +1,18 @@
 import NextAuth from "next-auth";
-
 import Google from "next-auth/providers/google";
 
-
 export const {
-handlers,
-signIn,
-signOut,
-auth
-}=NextAuth({
+  handlers,
+  signIn,
+  signOut,
+  auth
+} = NextAuth({
 
-providers:[
-
-Google({
-
-clientId:
-process.env.GOOGLE_CLIENT_ID,
-
-clientSecret:
-process.env.GOOGLE_CLIENT_SECRET
-
-})
-
-]
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ]
 
 });
