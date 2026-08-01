a2eb353388d9d8e9e639004640eb458c4f224870
vinhdau088-cloud <vinhdau088@gmail.com>
2026-08-01 21:10:38 +0800

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import Dashboard from "@/components/Dashboard";
 
 export default function Home() {
   return (
@@ -67,74 +68,10 @@ export default function Home() {
 
         </div>
 
+
+        <Dashboard />
+
       </main>
     </>
   );
-}
-
-
-
-<div className="grid">
-
-
-<div className="card">
-📚
-<h2>HSK</h2>
-<p>HSK 1 - HSK 6</p>
-</div>
-
-
-
-<div className="card">
-🇹🇼
-<h2>TOCFL</h2>
-<p>A1 - C2</p>
-</div>
-
-
-
-<div className="card">
-📝
-<h2>Luyện thi</h2>
-<p>Nghe - Đọc - Viết</p>
-</div>
-
-
-
-<div className="card">
-👥
-<h2>Cộng đồng</h2>
-<p>Chia sẻ học tập</p>
-</div>
-
-
-
-<div className="card">
-🃏
-<h2>Flashcard</h2>
-<p>Học từ vựng</p>
-</div>
-
-
-
-<div className="card">
-👤
-<h2>Hồ sơ</h2>
-<p>Tiến độ học</p>
-</div>
-
-
-
-</div>
-
-<Dashboard/>
-  
-
-</main>
-
-
-</>
-
-)
-
 }
