ad5be819931304171794aeffce787bfd3201672b
vinhdau088-cloud <vinhdau088@gmail.com>
2026-08-01 19:32:05 +0800

    Create Dashboard.jsx

---
+++ components/Dashboard.jsx
@@ -0,0 +1,72 @@
+export default function Dashboard(){
+
+return (
+
+<div>
+
+
+<div
+style={{
+background:"white",
+padding:25,
+borderRadius:25
+}}
+>
+
+<h2>
+🔥 Tiến độ hôm nay
+</h2>
+
+<p>
+HSK 3 - Bài 12
+</p>
+
+
+<div>
+███████░░ 70%
+</div>
+
+
+</div>
+
+
+
+
+
+<div
+style={{
+background:"white",
+padding:25,
+borderRadius:25,
+marginTop:20
+}}
+>
+
+<h2>
+📅 Kế hoạch hôm nay
+</h2>
+
+
+<p>
+✅ Học 20 từ mới
+</p>
+
+
+<p>
+⬜ Ôn Flashcard
+</p>
+
+
+<p>
+⬜ Làm 1 đề nghe
+</p>
+
+
+</div>
+
+
+</div>
+
+)
+
+}
