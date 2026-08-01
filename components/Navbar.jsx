5a3aa03ea4694e9bb1f68a5b8b514f928f4c1f30
vinhdau088-cloud <vinhdau088@gmail.com>
2026-08-01 18:37:42 +0800

    Create Navbar.jsx

---
+++ components/Navbar.jsx
@@ -0,0 +1,20 @@
+export default function Navbar(){
+
+return (
+
+<nav className="navbar">
+
+<div className="logo">
+🇨🇳 ChineseMaster AI
+</div>
+
+
+<div className="menu">
+☰
+</div>
+
+</nav>
+
+)
+
+}
